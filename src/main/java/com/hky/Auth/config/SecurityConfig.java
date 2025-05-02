b3f8a922ef7db874424fe23ab41302d7a4ca689d
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2025-05-02 15:44:49 +0530

    Updated URL 3

--- src/main/java/com/hky/Auth/config/SecurityConfig.java
+++ src/main/java/com/hky/Auth/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("https://authonly.netlify.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type","*"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
